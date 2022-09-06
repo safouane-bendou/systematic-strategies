a8abc16e4a32b53d86f07b9429c3add6b2b59e91
safouane-bendou <bendousafouane@gmail.com>
2022-09-06 10:24:17 +0200

    level 2 done

--- ConsoleApp1/Portfolio.Library/Portfolio.cs
+++ ConsoleApp1/Portfolio.Library/Portfolio.cs
@@ -20,49 +20,7 @@ namespace Portfolio.Library
         double capitalisationRiskFree { get; set; }
         double premium { get; set; }
 
-        public List<double> PortfolioValues(TestParameters testParameters, List<DataFeed> marketData)
-        {
-            //premium computing
-            DataFeed initialDataFeed = marketData[0];
-            DateTime previousDate = initialDataFeed.Date;
-            double[] initialSpots = PortfolioComputations.SpotsArray(initialDataFeed);
-            sharesDelta = PortfolioComputations.UpdateCompo(testParameters, initialDataFeed);
-            List<double> portfolio = new List<double>() { premium };
-            //portfolio.Add(premium);
-            foreach (DataFeed dataFeed in marketData.Skip(1))
-            {
-                //if (!dataFeed.Equals(initialDataFeed))
-                double[] spots = PortfolioComputations.SpotsArray(dataFeed);
-                capitalisationRiskFree = RiskFreeRateProvider.GetRiskFreeRateAccruedValue(previousDate, dataFeed.Date);
-                double portfolioValue = (premium - PortfolioComputations.riskyAssetsValue(sharesDelta, initialSpots)) * capitalisationRiskFree + PortfolioComputations.riskyAssetsValue(sharesDelta, spots);
-
-                //Don't forget the rebalancing ; discarded for now : if Rebalancing(dataFeed.Date)
-                sharesDelta = PortfolioComputations.UpdateCompo(testParameters, dataFeed);
-                //rebalancing done
-
-                initialSpots = spots;
-                previousDate = dataFeed.Date;
-                premium = portfolioValue;
-                portfolio.Add(portfolioValue);
-            }
-            return portfolio;
-        }
-
-
-        public static Dictionary<string, double> UpdateCompo(TestParameters testParameters, DataFeed dataFeed)
-        {
-            Pricer pricer = new Pricer(testParameters);
-            double timeToMaturity = MathDateConverter.ConvertToMathDistance(dataFeed.Date, testParameters.BasketOption.Maturity);
-            PricingResults priceResult = pricer.Price(timeToMaturity, SpotsArray(dataFeed));
-            Dictionary<string, double> resultedUpdate = new Dictionary<string, double>();
-            int counter = 0;
-            foreach (var shareId in dataFeed.PriceList.Keys)
-            {
-                resultedUpdate.Add(shareId, priceResult.Deltas[counter]);
-                counter++;
-            }
-            return resultedUpdate;
-        }
+       
 
     }
 }
--- ConsoleApp1/Portfolio.Library/PortfolioComputations.cs
+++ ConsoleApp1/Portfolio.Library/PortfolioComputations.cs
@@ -39,6 +39,50 @@ namespace Portfolio.Library
                 counter++;
             }
             return sumOfQuantities;
-        }      
+        }
+
+        public List<double> PortfolioValues(TestParameters testParameters, List<DataFeed> marketData)
+        {
+            //premium computing
+            DataFeed initialDataFeed = marketData[0];
+            DateTime previousDate = initialDataFeed.Date;
+            double[] initialSpots = SpotsArray(initialDataFeed);
+            sharesDelta = UpdateCompo(testParameters, initialDataFeed);
+            List<double> portfolio = new List<double>() { premium };
+            //portfolio.Add(premium);
+            foreach (DataFeed dataFeed in marketData.Skip(1))
+            {
+                //if (!dataFeed.Equals(initialDataFeed))
+                double[] spots = SpotsArray(dataFeed);
+                capitalisationRiskFree = RiskFreeRateProvider.GetRiskFreeRateAccruedValue(previousDate, dataFeed.Date);
+                double portfolioValue = (premium - riskyAssetsValue(sharesDelta, initialSpots)) * capitalisationRiskFree + riskyAssetsValue(sharesDelta, spots);
+
+                //Don't forget the rebalancing ; discarded for now : if Rebalancing(dataFeed.Date)
+                sharesDelta = UpdateCompo(testParameters, dataFeed);
+                //rebalancing done
+
+                initialSpots = spots;
+                previousDate = dataFeed.Date;
+                premium = portfolioValue;
+                portfolio.Add(portfolioValue);
+            }
+            return portfolio;
+        }
+
+
+        public static Dictionary<string, double> UpdateCompo(TestParameters testParameters, DataFeed dataFeed)
+        {
+            Pricer pricer = new Pricer(testParameters);
+            double timeToMaturity = MathDateConverter.ConvertToMathDistance(dataFeed.Date, testParameters.BasketOption.Maturity);
+            PricingResults priceResult = pricer.Price(timeToMaturity, SpotsArray(dataFeed));
+            Dictionary<string, double> resultedUpdate = new Dictionary<string, double>();
+            int counter = 0;
+            foreach (var shareId in dataFeed.PriceList.Keys)
+            {
+                resultedUpdate.Add(shareId, priceResult.Deltas[counter]);
+                counter++;
+            }
+            return resultedUpdate;
+        }
     }
 }
